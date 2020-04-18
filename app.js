d2aa2166546469f5164f995078add2bab1d448bb
unknown <nwosuchris5@gmail.com>
2020-04-18 03:26:17 -0700

    my final todolistapp

---
+++ app.js
@@ -0,0 +1,211 @@
+//jshint esversion : 6
+
+const express = require ("express");
+
+const bodyparser = require ("body-parser");
+
+const ejs = require ("ejs");
+
+const mongoose = require("mongoose");
+
+const _= require("lodash");
+
+
+const app = express();
+
+
+app.set('view engine','ejs');
+
+app.use(bodyparser.urlencoded({extended : true}));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB",{useUnifiedTopology:true});
+
+const itemsSchema={
+
+name : String
+    
+};
+
+const Item = mongoose.model("Item",itemsSchema);
+
+const item1 = new Item({
+
+    name : "Welcome to your todolist",
+});
+
+const item2 = new Item({
+
+    name : "Hit the + button to add a new item",
+});
+
+const item3 = new Item({
+
+    name : "<-- Hit this to delete an item.",
+});
+
+const defaultItems = [item1,item2,item3];
+
+const listSchema = {
+
+    name : String,
+
+    items:[itemsSchema]
+}
+
+ const List = mongoose.model("List",listSchema);
+
+app.get('/', function(req,res){
+
+
+    Item.find({},function(err,foundItems){
+    
+        if (foundItems.length===0){
+
+            Item.insertMany(defaultItems,function(err,foundItems){
+
+                if(err){
+                    console.log(err)
+                } else {
+                    console.log('Successfully saved')
+
+                }
+            });
+
+            res.redirect("/")
+
+        } else {
+
+        res.render("List",  {listTitle: "Today", newListItems: foundItems })
+
+    
+    }
+
+
+    });
+
+
+
+})
+
+    app.get("/:customListName",function(req,res){
+
+    const customListName = req.params.customListName;
+
+    List.findOne({name:customListName},function(err,foundList){
+
+        if(!err){
+            
+            if(!foundList){
+
+               const list =new List({
+
+                name:customListName,
+
+                items:defaultItems
+
+               });
+
+            list.save();
+
+
+            res.redirect("/"+ customListName);
+
+            } else{
+
+              res.render("list",  {listTitle:foundList.name, newListItems: foundList.items})
+            }
+        }
+
+    });
+
+    });
+
+
+
+
+
+
+app.post("/", function(req,res){
+
+const itemName = req.body.newItem;
+
+const listName = req.body.list;
+
+  
+        const item = new Item({
+        
+        name: itemName
+        });
+        
+        
+        if (listName==="Today"){ 
+
+        item.save();
+
+        res.redirect('/');
+
+
+    }  else {
+
+        List.findOne({name: listName},function(err,foundList){
+
+           foundList.items.push(item);
+
+            foundList.save();
+
+            res.redirect('/' + listName);
+        })
+
+}
+
+    });
+
+
+    
+    
+    app.post("/delete",function(req,res){
+    
+      const checkedItem = req.body.checkbox;
+
+      const listName = req.body.listName;
+
+
+          if(listName==="Today"){
+          
+      Item.findByIdAndRemove((checkedItem),function(err){
+
+        if(!err){
+
+            console.log("Successfully deleted checked item");
+
+            res.redirect("/");
+
+          } 
+
+        });
+
+    } else {
+
+     List.findOneAndUpdate({name:listName},{$pull:{items:{_id:checkedItem}}},function(err,foundList){
+
+        if(!err){
+
+            console.log("Succesfully deleted item.");
+
+            res.redirect("/"  + listName);
+        }
+        
+    })
+
+}
+
+    });
+
+
+app.listen(3000,()=>{
+
+    console.log("server is listening on  port 3000");
+})
+
